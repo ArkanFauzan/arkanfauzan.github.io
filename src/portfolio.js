16c2b1b1f3fb3f6e2c4f477744b031e90116b8e6
ArkanFauzan <arkan@icreativelabs.com>
2025-02-13 10:50:25 +0700

    Fix header font

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,7 +20,7 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Arkan Fauzan",
+  username: "Arkan Fauzan Ayyasyi",
   title: "Hi all, I'm Arkan",
   subTitle: emoji(
     "A passionate Web Developer 🚀 having an experiences of building Web applications with PHP / .NET Core / MySQL / MongoDB / Reactjs / Vuejs and some other cool libraries and frameworks."
